9164b65ccde5d14875afde35c113b89b7604852e
sai bhaskar <saibaaskar24091999@gmail.com>
2020-01-08 13:54:56 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'ekfchs9c69(31_=nu$87^x1ly(&)n5y9c=2#i&z&wqv1)a9p^-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myadjangoapp.herokuapp.com','127.0.0.1','viitblog.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1','viitblog.azurewebsites.net']
 
 
 # Application definition
@@ -71,10 +71,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
