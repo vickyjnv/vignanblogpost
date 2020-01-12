f0e4409f186abcbb6d076afbf020e20e0626b557
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-01-12 06:26:40 +0530

    Profile

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Posts
+from .models import Posts, UserProfile
 # Register your models here.
 
 admin.site.register(Posts)
+
+admin.site.register(UserProfile)
--- post/models.py
+++ post/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from groups.models import Com 
+from django.db.models.signals import post_save
 # Create your models here.
 
 
@@ -16,4 +17,23 @@ class Posts(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
+	def __str__(self):
+		return self.title
 
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.CharField(max_length=100, default='',blank = True)
+    website = models.URLField(default='',blank = True)
+    phone = models.IntegerField(default=0,blank = True)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
