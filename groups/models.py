b8a21823cb75c169224f583d2f8b083939e367f0
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-02-06 05:45:47 +0530

    Adding features to group

--- groups/models.py
+++ groups/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Com(models.Model):
 	name = models.CharField(max_length = 100, primary_key=True)
+	discripton = models.TextField(default="Test")
+	#user = models.ForeignKey(User, on_delete = models.CASCADE)
 	def __str__(self):
 		return self.name
 	def get_absolute_url(self):
