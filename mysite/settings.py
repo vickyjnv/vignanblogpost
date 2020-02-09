309f8790addef898fd721a4d3d7dc8d8a721a51a
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-02-09 07:53:35 +0530

    Static Images

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,6 +97,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_DIRS = (
+                os.path.join(BASE_DIR,'staticfiles'), 
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'post/media')
 
