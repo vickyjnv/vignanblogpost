a1943339cf2998a04c8e6b3327e685ef4df3f464
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-04-08 06:57:13 +0530

    UI Updare

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,6 +109,6 @@ LOGIN_URL = 'login'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pandupraveen143870@gmail.com'
+EMAIL_HOST_USER = 'saibaaskar24091999@gmail.com'
 EMAIL_HOST_PASSWORD = '*'
 EMAIL_PORT = 587
