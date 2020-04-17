a7c3d295392c9ca5e52e71adc56e16829bb9b07f
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-04-17 09:55:19 +0530

    Postsql

--- mysite/settings.py
+++ mysite/settings.py
@@ -62,14 +62,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'viitblog',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 import dj_database_url
+# print(dj_database_url)
 db_from_env = dj_database_url.config(conn_max_age=600)
+# print(db_from_env)
 DATABASES['default'].update(db_from_env)
 
 
