a55c10901f809064d616c37e4497d7d7c44faa32
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-08-22 08:14:32 +0530

    fix

--- post/views.py
+++ post/views.py
@@ -58,9 +58,10 @@ def register(request):
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
-			user = form.save(commit=False)
-			# user.is_active = False
 			user.save()
+			# user = form.save(commit=False)
+			# # user.is_active = False
+			# user.save()
 			# current_site = get_current_site(request)
 			# mail_subject = 'Activate your blog account.'
 			# message = render_to_string('acc_active_email.html', {
@@ -76,7 +77,7 @@ def register(request):
 			# messages.success(request, f'Account created for {username}!')
 			# messages.info(request, f'Please confirm your email address to complete the registration')
 			# return render(request,'email.html', {'to_email': to_email})
-            return redirect('index')
+			return redirect('index')
 	else:
 		form = UserRegisterForm()
 	return render(request, 'post/register.html', {'form': form })
